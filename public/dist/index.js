8b3c9ab045b914e22a63171968ccad326a0b6a64
Rogpe-Prog <paulo.rogpe@gmail.com>
2023-06-05 08:14:42 -0300

    commit

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,2 +1,35 @@
 "use strict";
-alert('Bora com projeto TS!!!');
+(function () {
+    var todo = {
+        description: 'todo',
+        done: false,
+    };
+    var reminder = {
+        description: 'remainder',
+        date: '15.12.2021',
+    };
+    var taskView = {
+        render: function (tasks) {
+            var taskList = document.getElementById("tasksList");
+            while (taskList === null || taskList === void 0 ? void 0 : taskList.firstChild) {
+                taskList.removeChild(taskList.firstChild);
+            }
+            tasks.forEach(function (task) {
+                var li = document.createElement("LI");
+                var textNode = document.createTextNode(JSON.stringify(task));
+                li.appendChild(textNode);
+                taskList === null || taskList === void 0 ? void 0 : taskList.appendChild(li);
+            });
+        }
+    };
+    var TaskController = function (view) {
+        var _a;
+        var tasks = [todo, reminder];
+        var handleEvent = function (event) {
+            event.preventDefault();
+            view.render(tasks);
+        };
+        (_a = document.getElementById("taskForm")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", handleEvent);
+    };
+    TaskController(taskView);
+})();
