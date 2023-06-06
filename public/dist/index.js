7212c95b9cf27340939fcc76dc5e2ce74829004f
Rogpe-Prog <paulo.rogpe@gmail.com>
2023-06-06 09:10:02 -0300

    commit

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,13 +1,38 @@
 "use strict";
 (function () {
-    var todo = {
-        description: 'todo',
-        done: false,
-    };
-    var reminder = {
-        description: 'remainder',
-        date: '15.12.2021',
-    };
+    var Reminder = /** @class */ (function () {
+        function Reminder(description, date, notifications) {
+            this.id = '';
+            this.dateCreated = new Date();
+            this.dateUpdated = new Date();
+            this.description = '';
+            this.date = new Date();
+            this.notifications = ['EMAIL'];
+            this.description = description;
+            this.date = date;
+            this.notifications = notifications;
+        }
+        Reminder.prototype.render = function () {
+            return JSON.stringify(this);
+        };
+        return Reminder;
+    }());
+    var Todo = /** @class */ (function () {
+        function Todo(description) {
+            this.id = '';
+            this.dateCreated = new Date();
+            this.dateUpdated = new Date();
+            this.description = '';
+            this.done = false;
+            this.description = description;
+        }
+        Todo.prototype.render = function () {
+            return JSON.stringify(this);
+        };
+        return Todo;
+    }());
+    var todo = new Todo('Todo criado com a classe');
+    var reminder = new Reminder('Remainder croiado com a classe', new Date(), ['EMAIL',]);
     var taskView = {
         render: function (tasks) {
             var taskList = document.getElementById("tasksList");
@@ -16,7 +41,7 @@
             }
             tasks.forEach(function (task) {
                 var li = document.createElement("LI");
-                var textNode = document.createTextNode(JSON.stringify(task));
+                var textNode = document.createTextNode(task.render());
                 li.appendChild(textNode);
                 taskList === null || taskList === void 0 ? void 0 : taskList.appendChild(li);
             });
